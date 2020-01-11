f84fd26a56faf8a168999394e712737360431594
lmm <meng@mys4s.cn>
2020-01-11 16:20:44 +0800

    demo

--- cnp/server.py
+++ cnp/server.py
@@ -1,1 +1,12 @@
+import socket
+
+class Server :
+    def __init__(self,config):
+        self._config = config
+        s = socket.socket()
+        host = socket.gethostname()
+        s.bind((host,config["server_port"]))
+        s.listen(5)
+        self.ptmap={}
+
 
